2dc0d427a7256f6ba850874af4e90661b822e7da
maluy <artem.evsiukov1@gmail.com>
2022-11-23 11:41:17 +0300

    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,31 +44,32 @@ var swiperSlider = function () {
       prevEl: '.swiper-button-prev'
     }
   });
-  var mySwiperMobail = new Swiper('.mySwiperMobail', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 8000
-    },
-    speed: 3500,
-    keyboard: {
-      enabled: true
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2
-      }
-    }
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  });
+
+  //   const mySwiperMobail = new Swiper('.mySwiperMobail', {
+  //     slidesPerView: 1,
+  //     spaceBetween: 30,
+  //     loop: true,
+  //     autoplay: {
+  //       delay: 8000,
+  //     },
+  //     speed: 3500,
+  //     keyboard: {
+  //       enabled: true,
+  //     },
+  //     breakpoints: {
+  //       768: {
+  //         slidesPerView: 2,
+  //       },
+  //     },
+  //     // pagination: {
+  //     //   el: '.swiper-pagination',
+  //     //   clickable: true,
+  //     // },
+  //     // navigation: {
+  //     //   nextEl: '.swiper-button-next',
+  //     //   prevEl: '.swiper-button-prev',
+  //     // },
+  //   });
 
   var init = function init() {};
   return {
